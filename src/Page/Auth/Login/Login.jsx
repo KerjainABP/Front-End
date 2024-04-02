c09d19b104c68b0740c4274191bf2c52bba4f756
Dio Siregar <dio.siregar03@gmail.com>
2024-04-02 19:20:25 +0700

    feat/eye

--- src/Page/Auth/Login/Login.jsx
+++ src/Page/Auth/Login/Login.jsx
@@ -1,17 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from '../../../Component/Navbar/Navbar'
-import {TbEyeClosed} from "react-icons/tb"
-import { Link } from 'react-router-dom'
+import {TbEye, TbEyeClosed} from "react-icons/tb"
 const Login = () => {
+    const [open, setOpen] = useState(true)
   return (
-    <div className=''>
+    <div className='font-popins'>
         <Navbar/>
-        <div className='bg-[#E5E5E5] h-[90vh]  flex flex-col gap-6 border justify-center items-center '>
-            <div className=''>
-                <p className='text-right text-[#104ACC] font-bold'>Apakah anda mencari karyawan? </p>
-                <div className='flex flex-col bg-white lg:px-[72px] lg:py-8 lg:w-[450px]'>
-                    <h1 className='mb-10 font-bold text-[36px]'>Masuk</h1>
-                    <div className='mb-10'>
+        <div className='bg-[#E5E5E5] h-[90vh]  flex flex-row border justify-center items-center'>
+            <div className=' '>
+                <h1>Apakah anda mencari karyawan? </h1>
+                <div className=' bg-white lg:px-[72px] lg:py-8 lg:w-[450px]'>
+                    <h1 className='font-bold text-[36px]'>Masuk</h1>
+                    <div>
                         <p>Email</p>
                         <div className=' border border-[#051A49] px-3 py-[6px] flex items-center rounded'>
                             <input className='outline-none w-[90%] ' type="email" name="email" id="email" />
@@ -20,27 +20,26 @@ const Login = () => {
                     <div className='mb-[45px]'>
                         <p>Password</p>
                         <div className=' border border-[#051A49] px-3 py-[6px] flex items-center rounded'>
-                            <input className='outline-none w-[90%] ' type="password" name="password" id="password" />
-                            <button className='text-[24px]'><TbEyeClosed className=''/></button>
+                            {open?
+                            (<input className='outline-none w-[90%] ' type="password" name="password" id="password" />):
+                            (<input className='outline-none w-[90%] ' type="text" name="password" id="password" />)
+
+                            }
+                            <button className='text-[24px]' onClick={()=> setOpen(!open)}>
+                                {open?(<TbEyeClosed className=''/>):(<TbEye className=''/>)}
+                            </button>
                         </div>
-                        <Link to='/' className='text-[#003EC8]'>Lupa Kata Sandi ? </Link>
                     </div>
                     <div className="flex flex-col items-center"> 
 
-                    <button type="button" className="text-white w-full bg-[#051A49] focus:ring-4 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 focus:outline-none">Masuk</button>
-                        <p>Tidak punya akun? <Link to='/' className='text-[#003EC8]'>daftar disini</Link></p>
+                        <button type="button" className="text-white w-full bg-[#051A49] focus:ring-4 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 focus:outline-none">Masuk</button>
+    
                     </div>
                 </div>
-
             </div>
-            <p className='font-bold text-[#051A49] text-[28px]'>Solusi Pertama Bagi Pengganguran</p>
         </div>
-        
     </div>
-
-
   )
 }
-
 
 export default Login
